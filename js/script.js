d189ea820c4f9d362880432021efac88523b1fd3
onetayjones <taylordjone@gmail.com>
2023-03-03 13:35:26 +0200

    adding in display backdrop

--- js/script.js
+++ js/script.js
@@ -81,6 +81,8 @@ async function displayMovieDetails() {
     const movieId = window.location.search.split('=')[1];
 
     const movie = await fetchAPIData(`movie/${movieId}`);
+    // Overlay for background Image
+    displayBackgroundImage('movie', movie.backdrop_path);
 
     const div = document.createElement('div');
     div.innerHTML = `
@@ -138,6 +140,28 @@ async function displayMovieDetails() {
     `;
 
     document.querySelector('#movie-details').appendChild(div);
+};
+
+// display Backdrop on Detail Pages
+function displayBackgroundImage(type, backgroundPath){
+    const overlayDiv = document.createElement('div');
+    overlayDiv.style.backgroundImage = `url(https://image.tmdb.org/t/p/original/${backgroundPath})`;
+    overlayDiv.style.backgroundSize = 'cover';
+    overlayDiv.style.backgroundPosition = 'center';
+    overlayDiv.style.backgroundRepeat = 'no-repeat';
+    overlayDiv.style.height = '100vh';
+    overlayDiv.style.width = '100vw';
+    overlayDiv.style.position = 'absolute';
+    overlayDiv.style.top = '0';
+    overlayDiv.style.left = '0';
+    overlayDiv.style.zIndex = '-1';
+    overlayDiv.style.opacity = '0.1';
+
+    if(type === 'movie'){
+        document.querySelector('#movie-details').appendChild(overlayDiv);
+    } else {
+        document.querySelector('#show-details').appendChild(overlayDiv);
+    }
 }
 
 // Fetch data from TMBD API
