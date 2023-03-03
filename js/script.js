4c158103bb1fbcf8909c235512dbe16f07b08aef
onetayjones <taylordjone@gmail.com>
2023-03-03 16:36:21 +0200

    adding in slider display

--- js/script.js
+++ js/script.js
@@ -227,6 +227,78 @@ function displayBackgroundImage(type, backgroundPath){
     }
 }
 
+// Display Slider Movies 
+async function displaySlider () {
+    const { results } = await fetchAPIData('movie/now_playing');
+
+    results.forEach((movie) => {
+        const div = document.createElement('div');
+        div.classList.add('swiper-slide');
+        div.innerHTML = `
+            <a href="movie-details.html?id=${movie.id}">
+            <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}" />
+            </a>
+            <h4 class="swiper-rating">
+            <i class="fas fa-star text-secondary"></i> ${movie.vote_average} / 10
+            </h4>
+      
+        `
+        // displays the innerHTML in the DOM
+        document.querySelector('.swiper-wrapper').appendChild(div);
+
+        initSwiper();
+    })
+}
+
+// Display Top Rated TV Shows
+async function displayTVSlider () {
+    const { results } = await fetchAPIData('/tv/top_rated');
+
+    results.forEach((show) => {
+        const div = document.createElement('div');
+        div.classList.add('swiper-slide');
+        div.innerHTML = `
+            <a href="shows.html?id=${show.id}">
+            <img src="https://image.tmdb.org/t/p/w500${show.poster_path}" alt="${show.name}" />
+            </a>
+            <h4 class="swiper-rating">
+            <i class="fas fa-star text-secondary"></i> ${show.vote_average} / 10
+            </h4>
+      
+        `
+        // displays the innerHTML in the DOM
+        document.querySelector('.swiper-wrapper').appendChild(div);
+
+        initSwiper();
+    })
+}
+
+function initSwiper(){
+    const swiper = new Swiper('.swiper', {
+        slidesPerView: 1,
+        spaceBetween: 30,
+        freeMode: true, 
+        loop: true,
+        autoplay: {
+            delay: 4000,
+            disableOnInteraction: false
+        },
+        breakpoints: {
+            500: {
+                slidesPerView: 2
+            }, 
+            700: {
+                slidesPerView: 3
+            },
+            1200: {
+                slidesPerView: 4
+            },
+        }
+    })
+};
+
+
+
 // Fetch data from TMBD API
 async function fetchAPIData(endpoint) {
     const API_KEY = '3e995e427fd2be80a3d1fd71b5d911e3';
@@ -271,9 +343,11 @@ function init() {
     switch(global.currentPage) {
     case '/':
     case '/index.html':
+        displaySlider();
         displayPopularMovies();
         break;
     case '/shows.html':
+        // displayTVSlider();
         displayPopularTV();
         break;
     case '/movie-details.html':
